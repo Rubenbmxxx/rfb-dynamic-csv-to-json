bb970775d1c42252a74b94ac8bce993f230ab6a1
rubenferrer <rubenferrer>
2024-01-23 15:26:39 +0100

    add files to the project and build first approach to identify nested column levels

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# rfb-dynamic-csv-to-json
-Parse CSV into a JSON file based on CSV column names
+# Dynamic CSV transformation into JSON
+Parse CSV into a JSON file based on CSV column names, making easy to add new JSON properties from a CSV file data source. 
+
+
+# Starting point
+
+## CSV file
+`files/MarketAvrgTicketSample.csv`
+
+
+## Desired JSON file
+`files/MarketAvrgTicketSample.json`
+
+
+# Process
+
+The aim of this script is to build dynamically a JSON file starting from a CSV file, following the rules below.
+
+Notice there are different column types:
+
+1. Column name where no underscore in it; indicates nesting is opened, so this there is no data for this column at the CSV.
+   (i.e. MarketsList)
+2. Column name where no underscore in it and there is data int this column at the CSV file; indicates it is a first level 
+ property. (i.e. schemaVersion)
+3. Column name contains underscore; that identifies from left side the origin and from right side the destiny. In addition,
+when a column appears with no data at the CSV file, indicates new nest opening, otherwise finding data in this column indicates
+it is a JSON propertie itself. (i.e. MarketsList_Market and Market_isoCod)
+
---
+++ src/csv_to_json_dynamic.py
@@ -0,0 +1,98 @@
+import json
+import pandas as pd
+from datetime import datetime
+
+
+def parse_csv_to_json(columns):
+    json_schema = {}
+
+    # Iterate CSV columns identifying "_" and prefix/suffix
+    for column in columns:
+        parts = column.split('_')
+        if len(parts) == 1:
+            json_schema[column] = []
+            prefix = parts[0] + '_'
+        else:
+            prefix = parts[1] + '_'
+
+        # Loop again over columns using previously identified CSV column name to create new property or assign child
+        # property if exists
+        for nest in columns:
+            if nest.startswith(prefix):
+                if column not in json_schema.keys():
+                    json_schema[column] = []  # new level
+                json_schema[column].append(nest)  # Nested level
+
+    return json_schema
+
+
+def csv_to_json(pdf_csv, json_schema, grouped_df=None):
+    json_formatted = []
+    level = None
+    if not level:
+        level = [x for x in json_schema.keys() if len(x.split("_")) == 1]
+        grouped_df = pdf_csv.groupby(by=level, dropna=False)
+    elif not grouped_df:
+        grouped_df = pdf_csv.groupby(by=level, dropna=False)
+
+    for values, schema in grouped_df:
+        json_data = {}
+
+        for i, key in enumerate(level):
+            if not json_schema[key]:  # json_schema[key] is None does not work
+                json_data[key] = values[i]
+            elif key.endswith("List"):
+                json_data[key] = []
+                slc_cols, grp_cols = extract_group_columns(json_schema=json_schema, nested_levels=json_schema[key])
+                sub_data = schema[slc_cols]
+                # subloop = 1
+                grp_sub_data = sub_data.groupby([col for col in slc_cols if col not in grp_cols])
+                for sk, sv in grp_sub_data:
+                    gen_json = set_data(json_schema, key, sk, sv)
+                    json_data[key].append(gen_json)
+            else:
+                json_data[key] = {}
+                for attr in json_schema[key]:
+                    json_data[key][attr] = schema[attr].values[0]
+        json_formatted.append(json_data)
+    return json_formatted
+
+
+def set_data(json_schema, key, sk, sv):
+    # for j in json_schema:
+    return 1
+
+
+def extract_group_columns(json_schema, nested_levels):
+    slc_columns = []
+    grp_columns = []
+
+    for level in nested_levels:
+        if level in json_schema:
+            sub_level = json_schema[level]
+            slc, grp = extract_group_columns(json_schema, sub_level)
+            grp_columns.extend(grp)
+            slc_columns.extend(slc)
+        else:
+            if level.split("_")[0].endswith("List"):
+                grp_columns.append(level)
+            slc_columns.append(level)
+    return slc_columns, grp_columns
+
+
+if __name__ == '__main__':
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+
+    # Working directories related to current project structure:
+    csv_file = 'files/MarketAvrgTicketSample.csv'
+    json_file_path = f'files/output_{timestamp}.json'
+
+    # Read CSV file
+    raw_df = pd.read_csv(csv_file, sep=";")
+    csv_columns = list(raw_df.columns)
+
+    # Parse CSV file into JSON dict:
+    parsed_json_dict = parse_csv_to_json(csv_columns)
+
+    # Run transformation:
+    csv_to_json(pdf_csv=raw_df, json_schema=parsed_json_dict)
